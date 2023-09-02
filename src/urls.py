5492d49ffa78d5ab508dfd7e3a7ad6db3edf8fa6
Bruno Peixoto <brunolnetto@gmail.com>
2023-09-02 19:54:09 -0300

    refac/ remove reset password for a while

--- src/urls.py
+++ src/urls.py
@@ -24,10 +24,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
