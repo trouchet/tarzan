8e688b636f1eb03f0a8fcf013a776974edc768a2
Bruno Peixoto <brunolnetto@gmail.com>
2023-09-04 17:47:01 -0300

    refac/ remove django and import environ

--- manage.py
+++ manage.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import sys
 import os
-from sys import argv
-import django
 
+from os import environ
+from sys import argv
+
+# Add the project directory to sys.path
+project_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, project_directory)
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
+    environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
