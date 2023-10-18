0ce7da258d347f75804959a63e89a1be777e57e7
Bruno Peixoto <brunolnetto@gmail.com>
2023-10-18 09:05:01 -0300

    lint/ fix further suggestions

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,6 +4,57 @@ Module Docstring:
 This module provides fixtures for setting up test data.
 """
 
+import pytest
+from django.test import RequestFactory, Client
+from src.middleware import RedirectMiddleware
 from src.middleware import RedirectMiddleware
 
 redirect_middleware = RedirectMiddleware(get_response=None)
+
+@pytest.fixture
+def middleware():
+    """
+    Fixture for creating an instance of the RedirectMiddleware. This middleware is typically used
+    to redirect requests to a specific URL or perform some processing. In this fixture, a lambda
+    function is used to create a simple instance of the middleware that returns the string "42" as
+    a placeholder response.
+
+    Usage:
+        You can use this fixture in your test cases to obtain an instance of the RedirectMiddleware.
+
+    Example:
+        middleware_instance = middleware()
+    """
+    return RedirectMiddleware(lambda request: "42")
+
+
+@pytest.fixture
+def client():
+    """
+    Fixture for creating an instance of the Django Client. The Client is a test client provided by
+    Django for simulating HTTP requests. It allows you to send requests to your Django application 
+    and retrieve the responses for testing purposes.
+
+    Usage:
+        You can use this fixture in your test cases to obtain an instance of the Django Client.
+
+    Example:
+        test_client = client()
+    """
+    return Client()
+
+
+@pytest.fixture
+def rf():
+    """
+    Fixture for creating an instance of the Django RequestFactory. The RequestFactory is a utility
+    for creating request objects, which can be used to simulate HTTP requests in Django test cases.
+    This fixture is commonly used to create request objects for testing middleware and views.
+
+    Usage:
+        You can use this fixture in your test cases to obtain an instance of the RequestFactory.
+
+    Example:
+        request_factory = rf()
+    """
+    return RequestFactory()
--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -20,59 +20,7 @@ the provided test cases. Actual behavior may vary depending on the real implemen
 middleware.
 """
 
-import pytest
-from django.test import RequestFactory, Client
-from src.middleware import RedirectMiddleware
-
-
-@pytest.fixture
-def middleware():
-    """
-    Fixture for creating an instance of the RedirectMiddleware. This middleware is typically used
-    to redirect requests to a specific URL or perform some processing. In this fixture, a lambda
-    function is used to create a simple instance of the middleware that returns the string "42" as
-    a placeholder response.
-
-    Usage:
-        You can use this fixture in your test cases to obtain an instance of the RedirectMiddleware.
-
-    Example:
-        middleware_instance = middleware()
-    """
-    return RedirectMiddleware(lambda request: "42")
-
-
-@pytest.fixture
-def client():
-    """
-    Fixture for creating an instance of the Django Client. The Client is a test client provided by
-    Django for simulating HTTP requests. It allows you to send requests to your Django application and
-    retrieve the responses for testing purposes.
-
-    Usage:
-        You can use this fixture in your test cases to obtain an instance of the Django Client.
-
-    Example:
-        test_client = client()
-    """
-    return Client()
-
-
-@pytest.fixture
-def rf():
-    """
-    Fixture for creating an instance of the Django RequestFactory. The RequestFactory is a utility
-    for creating request objects, which can be used to simulate HTTP requests in Django test cases.
-    This fixture is commonly used to create request objects for testing middleware and views.
-
-    Usage:
-        You can use this fixture in your test cases to obtain an instance of the RequestFactory.
-
-    Example:
-        request_factory = rf()
-    """
-    return RequestFactory()
-
+from .fixtures import client, middleware, rf 
 
 REDIRECT_URL = "/api/"
 VALID_URL = REDIRECT_URL
@@ -89,12 +37,12 @@ def test_valid_url(middleware, rf):
     Test Steps:
     1. Creates a request object for a valid URL using the RequestFactory.
     2. Passes the request through the middleware.
-    3. Asserts that the response from the middleware is equal to "42," indicating that the middleware
-    did not perform a redirect.
+    3. Asserts that the response from the middleware is equal to "42," indicating that the 
+    middleware did not perform a redirect.
 
     Note:
-        This test assumes that the middleware should return the string "42" for valid URLs. The actual
-        behavior may depend on the real implementation of the middleware.
+        This test assumes that the middleware should return the string "42" for valid URLs. The 
+        actual behavior may depend on the real implementation of the middleware.
     """
     # Test a valid URL should not be redirected
     request = rf.get(VALID_URL)
